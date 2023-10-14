a87bfe2c235a480be7609d05016d4b0759919351
MothOfFire <1748972220@qq.com>
2023-10-14 14:45:34 +0800

    feat:实现了路由懒加载

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 import type { RouteObject } from 'react-router-dom';
 
-import Discover from '@/views/discover';
-import My from '@/views/my';
-import Focus from '@/views/focus';
-import Download from '@/views/download';
+// import Discover from '@/views/discover';
+// import My from '@/views/my';
+// import Focus from '@/views/focus';
+// import Download from '@/views/download';
+
+// 路由懒加载组件
+const Discover = lazy(() => import('@/views/discover'));
+const My = lazy(() => import('@/views/my'));
+const Focus = lazy(() => import('@/views/focus'));
+const Download = lazy(() => import('@/views/download'));
 
 const routes: RouteObject[] = [
   {
