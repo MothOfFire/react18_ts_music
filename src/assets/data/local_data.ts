6dd4eb2351983e0297604ba8114c4332f3f39ca1
MothOfFire <1748972220@qq.com>
2023-10-17 23:37:31 +0800

    feat(discover):实现了发现音乐组件的导航栏组件

--- src/assets/data/local_data.ts
+++ src/assets/data/local_data.ts
@@ -17,10 +17,48 @@ export const discoverMenu = [
   },
   {
     title: '歌手',
-    link: '/discover/singer'
+    link: '/discover/artist'
   },
   {
     title: '新碟上架',
     link: '/discover/album'
   }
-]
+];
+
+export const footerLinks = [
+  {
+    title: '服务条款',
+    link: 'https://st.music.163.com/official-terms/service'
+  },
+  {
+    title: '隐私政策',
+    link: 'https://st.music.163.com/official-terms/privacy'
+  },
+  {
+    title: '儿童隐私政策',
+    link: 'https://st.music.163.com/official-terms/children'
+  },
+  {
+    title: '版权控诉指引',
+    link: 'https://music.163.com/st/staticdeal/complaints.html'
+  },
+  {
+    title: '意见反馈',
+    link: '#'
+  }
+];
+
+export const footImages = [
+  {
+    link: 'https://music.163.com/st/userbasic#/auth'
+  },
+  {
+    link: 'https://music.163.com/recruit'
+  },
+  {
+    link: 'https://music.163.com/web/reward'
+  },
+  {
+    link: 'https://music.163.com/uservideo#/plan'
+  }
+];
